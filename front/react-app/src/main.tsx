72d16d361c2be2e0038fdf95ca362a12376d6076
takuya0202 <shimozono.t.24kdgn@gmail.com>
2026-02-13 00:16:18 +0900

    react-router追加

--- front/react-app/src/main.tsx
+++ front/react-app/src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './Home.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Detail from './Detail'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Home />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path='/projects' element={<Home />} />
+        <Route path='/projects/:projectId' element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
+ 
